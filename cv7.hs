d9869b22d7bfd8ffbd56a5d5c8de359f51e0429a
Martin Pilát <martin.pilat@gmail.com>
2025-04-02 17:50:21 +0200

    Pridano sedme cviceni

---
+++ cv7.hs
@@ -0,0 +1,22 @@
+-- komentar
+factorial :: Integer -> Integer
+--factorial :: (Num p, Ord p) => p -> p
+factorial 0 = 1
+factorial n | n > 0     = n * factorial (n-1)
+            | otherwise = 0 
+
+--pricti1 :: Num a => a -> a
+pricti1 x = x+1
+
+myMap f [] = []
+myMap f (x:xs) = f x:myMap f xs
+
+myZip [] _          = []
+myZip _ []          = []
+myZip (x:xs) (y:ys) = (x, y):myZip xs ys
+
+pricitatko1 = myMap pricti1
+
+main = print $ myMap pricti1 [1,2,3]
+
+--prime n = all [2..(n-1)]
